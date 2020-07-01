493ce95a32dc184b14a128237375d6584da7a3e8
Enrique Benitez <enriquebenitezjr@gmail.com>
2020-07-01 17:47:57 -0500

    Remove OSS alert

--- components/alert.js
+++ components/alert.js
@@ -1,6 +1,5 @@
 import Container from './container'
 import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
 
 export default function Alert({ preview }) {
   return (
@@ -12,7 +11,7 @@ export default function Alert({ preview }) {
     >
       <Container>
         <div className="py-2 text-center text-sm">
-          {preview ? (
+          {preview && (
             <>
               This is page is a preview.{' '}
               <a
@@ -22,17 +21,6 @@ export default function Alert({ preview }) {
                 Click here
               </a>{' '}
               to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/zeit/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
             </>
           )}
         </div>
